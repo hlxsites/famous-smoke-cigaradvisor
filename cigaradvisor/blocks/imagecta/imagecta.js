a997d51a5088376f9ef4eb1c22237057ce966416
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2023-11-30 12:38:55 -0500

    Update imagecta.js

--- cigaradvisor/blocks/imagecta/imagecta.js
+++ cigaradvisor/blocks/imagecta/imagecta.js
@@ -1,27 +1,27 @@
 export default function decorate(block) {
   const anchor = document.createElement('a');
   [...block.children].forEach((row) => {
-    var link;
+    let link;
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         anchor.append(pic);
       }
-      if (col.textContent.toLowerCase() !== "image" && col.textContent.toLowerCase() !== "link") {
+      if (col.textContent.toLowerCase() !== 'image' && col.textContent.toLowerCase() !== 'link') {
         link = col.textContent;
       }
     });
-    anchor.setAttribute("href", link);
-
-    const children = block.children;
-
-    // Loop through the children and remove those that are not anchor tags
-    for (let i = children.length - 1; i >= 0; i--) {
-      if (children[i].tagName !== 'A') {
-        console.log(children[i].tagName);
-        block.removeChild(children[i]);
-      }
-    }
+    anchor.setAttribute('href', link);
   });
   block.append(anchor);
+  const children = block.children;
+
+  // Loop through the children and remove those that are not anchor tags
+  Array.from(children).forEach((child) => {
+    if (child.tagName !== 'A') {
+      console.log(child.tagName);
+      block.removeChild(child);
+    }
+  });
+
 }

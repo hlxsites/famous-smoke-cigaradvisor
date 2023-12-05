1b7042729fcdb5041142d7ef9c3be0e5cb04786d
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2023-12-05 00:06:08 -0500

    link fixes

--- cigaradvisor/blocks/article-teaser/article-teaser.js
+++ cigaradvisor/blocks/article-teaser/article-teaser.js
@@ -2,25 +2,23 @@ import { createOptimizedPicture, readBlockConfig, getMetadata } from '../../scri
 
 function formatDate(originalDateString) {
   const utcDateString = new Date((originalDateString - 25569) * 86400 * 1000);
-  console.log(utcDateString);
   const utcDate = new Date(utcDateString);
-  console.log(utcDate);
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const day = utcDate.getUTCDate();
   const month = months[utcDate.getUTCMonth()];
   const year = utcDate.getUTCFullYear().toString().slice(-2); // Get last two digits of the year
   const formattedDate = `${day} ${month} ${year}`;
-  return formattedDate;
+  const dateTimeAttribute = `${utcDate.getUTCFullYear()}-${String(utcDate.getUTCMonth() + 1).padStart(2, '0')}-${String(utcDate.getUTCDate()).padStart(2, '0')}`;
+  return `${formattedDate}|${dateTimeAttribute}`;
 }
 
 async function fetchData(url) {
-  console.log("URL", url);
   const resp = await fetch(url);
+  let jsonDataJson = '';
   if (resp.ok) {
-    const jsonDataJson = await resp.json();
-    console.log(jsonDataJson);
-    return jsonDataJson.data;
+    jsonDataJson = await resp.json();
   }
+  return jsonDataJson.data;
 }
 
 export default async function decorate(block) {
@@ -32,13 +30,12 @@ export default async function decorate(block) {
   const trimmedURL = url.pathname;
   const fetchUrl = `${window.hlx.codeBasePath}/drafts/Kailas/pagemeta.json`;
   const teaserContent = await fetchData(fetchUrl);
-  const articleInfo = teaserContent.find(obj => obj.path === trimmedURL);
+  const articleInfo = teaserContent.find((obj) => obj.path === trimmedURL);
   const categoryListUrl = `${window.hlx.codeBasePath}/drafts/Kailas/category/category-list.json`;
   const categoryListData = await fetchData(categoryListUrl);
-  console.log('categoryListData', categoryListData);
   const articlePath = articleInfo.path;
   fetch(articlePath)
-    .then(response => {
+    .then((response) => {
       // Check if the request was successful (status code 200)
       if (response.ok) {
         // Convert the response to text
@@ -46,21 +43,17 @@ export default async function decorate(block) {
       }
       throw new Error('Network response was not ok.');
     })
-    .then(html => {
+    .then((html) => {
       // Create a new HTML document using DOMParser
       const parser = new DOMParser();
       doc = parser.parseFromString(html, 'text/html');
-
-
-      console.log('doc', doc);
-      console.log(doc.head);
       const articleCategory = getMetadata('category', doc);
-      console.log('articleCategory', articleCategory);
-      const articleCategoryInfo = categoryListData.find(obj => obj.category === articleCategory);
-      console.log('articleCategoryInfo', articleCategoryInfo);
+      const articleCategoryInfo = categoryListData.find((obj) => obj.category === articleCategory);
       const articleCategoryLink = articleCategoryInfo.categoryLink;
-      console.log('articleCategoryLink', articleCategoryLink);
-      const formattedDate = formatDate(articleInfo.publishedDate);
+      const formattedDate = formatDate(articleInfo.publishedDate).split('|')[0];
+      const datetimeAttr = formatDate(articleInfo.publishedDate).split('|')[1];
+      const authorNameHyphenSeparated = articleInfo.author.split(' ').join('-');
+      const authorLink = `${window.hlx.codeBasePath}/author/drafts/${authorNameHyphenSeparated.toLowerCase()}`;
       block.innerHTML = `
         <article onclick="" class="article article--thumbnail">
           <a class="article__category tag" href="${articleCategoryLink}" data-category="${articleCategory}" title="${articleCategory}">
@@ -74,13 +67,13 @@ export default async function decorate(block) {
                 <a class="article__titleLink article__link" href="${articleInfo.path}" title="${articleInfo.title}">${articleInfo.title}</a>
                   </h2>
                   <div class="article__meta">
-                <a class="article__authorLink" href="${articleInfo.authorLink}" title="By ${articleInfo.author}">By ${articleInfo.author}</a>
-                <time class="article__pubdate" datetime="2023-11-10">${formattedDate}</time>
+                <a class="article__authorLink" href="${authorLink}" title="By ${articleInfo.author}">By ${articleInfo.author}</a>
+                <time class="article__pubdate" datetime="${datetimeAttr}">${formattedDate}</time>
           </div>
           </articleheader>
           <div class="article__preview">
             <div class="article__excerpt">
-                <p><span class="rt-reading-time" style="display: block;"><span class="rt-label rt-prefix">Reading Time: </span> <span class="rt-time">${articleInfo.readingTime}</span></span> ${articleInfo.description}</p>
+                <p><span class="rt-reading-time" style="display: block;"><span class="rt-label rt-prefix">Reading Time: </span> <span class="rt-time">${articleInfo.readingTime}</span></span> ${articleInfo.articleBlurb}</p>
             </div>
             <a class="article__readMore article__link read_more" href="${articleInfo.path}" title="Read More">Read More</a>
           </div>
@@ -88,7 +81,7 @@ export default async function decorate(block) {
         </article>
         `;
     })
-    .catch(error => {
+    .catch((error) => {
       // Handle any errors that occurred during the fetch
       console.error('There was a problem with the fetch operation:', error);
     });

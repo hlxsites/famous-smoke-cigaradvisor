9c71b5e2155be9fe213ee75b13d4ad5dfb2ca207
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2023-12-17 23:40:27 -0500

    initial

---
+++ cigaradvisor/blocks/authorcard/authorcard.js
@@ -0,0 +1,74 @@
+import { readBlockConfig, createOptimizedPicture } from '../../scripts/aem.js';
+import { isExternal, fetchData } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  const configs = readBlockConfig(block);
+  console.log(configs);
+  const { title } = configs;
+  const buttonLabel = configs['button-label'];
+  const buttonLink = configs['button-link'];
+  const { authors } = configs;
+  console.log(buttonLabel);
+  const leftDiv = document.createElement('div');
+  leftDiv.classList.add('left-column');
+  const authorLeftContentWrapper = document.createElement('div');
+  authorLeftContentWrapper.classList.add('author-left-content-wrapper');
+  title.forEach((titleLine) => {
+    const h2Tag = document.createElement('h2');
+    h2Tag.innerHTML = titleLine;
+    authorLeftContentWrapper.append(h2Tag);
+  });
+  const anchorWrapper = document.createElement('p');
+  const buttonAnchor = document.createElement('a');
+  buttonAnchor.innerHTML = buttonLabel;
+  buttonAnchor.setAttribute('href', buttonLink);
+  buttonAnchor.setAttribute('target', isExternal(buttonLink) ? '_blank' : '_self');
+  buttonAnchor.setAttribute('title', buttonLabel);
+  buttonAnchor.classList.add('button');
+  anchorWrapper.append(buttonAnchor);
+  authorLeftContentWrapper.append(anchorWrapper);
+  leftDiv.append(authorLeftContentWrapper);
+  block.replaceChildren(leftDiv);
+
+  const rightDiv = document.createElement('div');
+  rightDiv.classList.add('right-column');
+  const fetchUrl = `${window.hlx.codeBasePath}/drafts/Kailas/pagemeta.json`;
+  const authorContent = await fetchData(fetchUrl);
+  console.log(authorContent);
+  const authorsObj = [];
+  [...authors].forEach((authorPage) => {
+    let authorPath;
+    try {
+      const url = new URL(authorPage);
+      authorPath = url.pathname;
+    } catch (error) {
+      authorPath = authorPage;
+    }
+    const authorInfo = authorContent.find((obj) => obj.path === authorPath);
+    if (authorInfo) {
+      const authorDetails = {
+        page: authorInfo.path,
+        name: authorInfo.authorName,
+        image: authorInfo.authorImage,
+      };
+      authorsObj.push(authorDetails);
+    }
+  });
+  console.log(authorsObj);
+  const authorWrapperSection = document.createElement('section');
+  authorWrapperSection.classList.add('author-wrapper');
+  authorWrapperSection.innerHTML = '';
+  [...authorsObj].forEach((author) => {
+    authorWrapperSection.innerHTML
+            += `<div class="author-content">
+    <div class="overlay-image">
+    ${createOptimizedPicture(author.image).outerHTML}
+    <div class="overlay-content">
+    <p class="align-center"><a href="${author.path}">${author.name}</a></p>
+    </div>
+    </div>
+    </div>`;
+  });
+  rightDiv.replaceChildren(authorWrapperSection);
+  block.append(rightDiv);
+}
--- cigaradvisor/scripts/scripts.js
+++ cigaradvisor/scripts/scripts.js
@@ -103,6 +103,20 @@ export function isExternal(path) {
 }
 
 /**
+ * Simple funtion to fetch json data from a spreadsheet
+ * @param {*} url
+ * @returns jsonData
+ */
+export async function fetchData(url) {
+  const resp = await fetch(url);
+  let jsonData = '';
+  if (resp.ok) {
+    jsonData = await resp.json();
+  }
+  return jsonData.data;
+}
+
+/**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
  */

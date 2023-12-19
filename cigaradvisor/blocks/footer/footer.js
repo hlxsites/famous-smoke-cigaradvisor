8933141e6d28e243ec53329abfdd2a15c146b5d2
Luca <adobe@gituser.de>
2023-12-19 17:19:39 +0100

    add basic footer implementation

--- cigaradvisor/blocks/footer/footer.js
+++ cigaradvisor/blocks/footer/footer.js
@@ -7,15 +7,43 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
-  block.textContent = '';
 
   // load footer fragment
   const footerPath = footerMeta.footer || '/cigaradvisor/footer';
-  const fragment = await loadFragment(footerPath);
+  const footerContent = await loadFragment(footerPath);
+  const currentYear = new Date().getFullYear();
+  footerContent.innerHTML = footerContent.innerHTML.replaceAll('{year}', currentYear.toString());
 
-  // decorate footer DOM
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  // decorate footer sections
+  const navContainer = footerContent.querySelector('div.footer-nav > .default-content-wrapper');
+  if (navContainer && navContainer.childNodes.length) {
+    navContainer.classList.add('nav-container');
 
-  block.append(footer);
+    let currentElement = navContainer.firstElementChild;
+    let nextElement;
+    while (currentElement) {
+      // create section
+      const section = document.createElement('div');
+      section.classList.add('nav-section');
+      if (currentElement.tagName === 'H1') {
+        section.classList.add('with-heading');
+      }
+
+      // populate section
+      do {
+        nextElement = currentElement.nextSibling;
+        section.appendChild(currentElement);
+        currentElement = nextElement;
+      } while (nextElement && nextElement.tagName !== 'H1');
+
+      // add section to container
+      if (nextElement) {
+        navContainer.insertBefore(section, nextElement);
+      } else {
+        navContainer.appendChild(section);
+      }
+    }
+  }
+
+  block.innerHTML = footerContent.innerHTML;
 }

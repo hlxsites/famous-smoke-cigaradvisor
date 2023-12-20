f5bc4838a1334c64557fcba04f7fd3a61bcd9cda
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2023-12-20 09:25:20 -0500

    Update contest-form.js

--- cigaradvisor/blocks/contest-form/contest-form.js
+++ cigaradvisor/blocks/contest-form/contest-form.js
@@ -3,10 +3,13 @@ export default async function decorate(block) {
   iframeWrapper.id = 'vsscript_148868_314195';
   block.replaceChildren(iframeWrapper);
   const p = document.createElement('p');
-  const script = document.createElement('script');
-  script.async = true;
-  script.type = 'text/javascript';
-  script.src = 'https://app.viralsweep.com/vsa-widget-bedfd7-148868.js?sid=148868_314195';
-  p.appendChild(script);
+  function loadScript() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.type = 'text/javascript';
+    script.src = 'https://app.viralsweep.com/vsa-widget-bedfd7-148868.js?sid=148868_314195';
+    p.appendChild(script);
+  }
   block.appendChild(p);
+  setTimeout(loadScript, 3000);
 }

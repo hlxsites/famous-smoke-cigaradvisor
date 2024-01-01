510b07d8029c2a1578a98db6ea087e6b9aff885e
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-01-01 13:43:30 -0500

    header

--- cigaradvisor/blocks/header/header.js
+++ cigaradvisor/blocks/header/header.js
@@ -95,52 +95,28 @@ export default async function decorate(block) {
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta).pathname : '/cigaradvisor/nav';
   const fragment = await loadFragment(navPath);
-
   // decorate nav DOM
   const nav = document.createElement('nav');
   nav.id = 'nav';
-  while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
-
-  const classes = ['brand', 'sections', 'tools'];
-  classes.forEach((c, i) => {
-    const section = nav.children[i];
-    if (section) section.classList.add(`nav-${c}`);
-  });
-
-  const navBrand = nav.querySelector('.nav-brand');
-  const brandLink = navBrand.querySelector('.button');
-  if (brandLink) {
-    brandLink.className = '';
-    brandLink.closest('.button-container').className = '';
-  }
-
-  const navSections = nav.querySelector('.nav-sections');
-  if (navSections) {
-    navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
-      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-      navSection.addEventListener('click', () => {
-        if (isDesktop.matches) {
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          toggleAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-        }
-      });
-    });
-  }
-
-  // hamburger for mobile
-  const hamburger = document.createElement('div');
-  hamburger.classList.add('nav-hamburger');
-  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-      <span class="nav-hamburger-icon"></span>
-    </button>`;
-  hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-  nav.prepend(hamburger);
-  nav.setAttribute('aria-expanded', 'false');
-  // prevent mobile nav behavior on window resize
-  toggleMenu(nav, navSections, isDesktop.matches);
-  isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
-
+  const topNav = document.createElement('div');
+  topNav.className = 'top-nav';
+  const topNavContent = document.createElement('div');
+  topNavContent.className = 'top-nav-content';
+  const topNavLeft = fragment.children[0];
+  topNavLeft.classList.add('top-nav-left');
+  topNavContent.append(topNavLeft);
+  const brand = document.createElement('div');
+  brand.innerHTML = `<a href="https://www.famous-smoke.com/cigaradvisor" rel="home" class="layout__logo lg-andUp" title="Cigar Advisor Homepage">
+  <img src="https://www.famous-smoke.com/cigaradvisor/wp-content/themes/CigarAdvisor/assets/images/logo.png?w=160" alt="Cigar Advisor Logo">
+  </a>`;
+  brand.className = 'brand-logo';
+  topNavContent.append(brand);
+  const topNavRight = fragment.children[0];
+  topNavRight.classList.add('top-nav-right');
+  topNavContent.append(topNavRight);
+  topNav.append(topNavContent);
+  nav.append(topNav);
+  nav.append(fragment.children[0]);
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);

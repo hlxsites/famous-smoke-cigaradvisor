4961020300b1efeaffcad72bffd675d68771531b
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-01-03 12:47:26 -0500

    desktop complete

--- cigaradvisor/blocks/header/header.js
+++ cigaradvisor/blocks/header/header.js
@@ -4,88 +4,6 @@ import { loadFragment } from '../fragment/fragment.js';
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
-function closeOnEscape(e) {
-  if (e.code === 'Escape') {
-    const nav = document.getElementById('nav');
-    const navSections = nav.querySelector('.nav-sections');
-    const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
-    if (navSectionExpanded && isDesktop.matches) {
-      // eslint-disable-next-line no-use-before-define
-      toggleAllNavSections(navSections);
-      navSectionExpanded.focus();
-    } else if (!isDesktop.matches) {
-      // eslint-disable-next-line no-use-before-define
-      toggleMenu(nav, navSections);
-      nav.querySelector('button').focus();
-    }
-  }
-}
-
-function openOnKeydown(e) {
-  const focused = document.activeElement;
-  const isNavDrop = focused.className === 'nav-drop';
-  if (isNavDrop && (e.code === 'Enter' || e.code === 'Space')) {
-    const dropExpanded = focused.getAttribute('aria-expanded') === 'true';
-    // eslint-disable-next-line no-use-before-define
-    toggleAllNavSections(focused.closest('.nav-sections'));
-    focused.setAttribute('aria-expanded', dropExpanded ? 'false' : 'true');
-  }
-}
-
-function focusNavSection() {
-  document.activeElement.addEventListener('keydown', openOnKeydown);
-}
-
-/**
- * Toggles all nav sections
- * @param {Element} sections The container element
- * @param {Boolean} expanded Whether the element should be expanded or collapsed
- */
-function toggleAllNavSections(sections, expanded = false) {
-  sections.querySelectorAll('.nav-sections .default-content-wrapper > ul > li').forEach((section) => {
-    section.setAttribute('aria-expanded', expanded);
-  });
-}
-
-/**
- * Toggles the entire nav
- * @param {Element} nav The container element
- * @param {Element} navSections The nav sections within the container element
- * @param {*} forceExpanded Optional param to force nav expand behavior when not null
- */
-function toggleMenu(nav, navSections, forceExpanded = null) {
-  const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
-  const button = nav.querySelector('.nav-hamburger button');
-  document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
-  nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(navSections, expanded || isDesktop.matches ? 'false' : 'true');
-  button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
-  // enable nav dropdown keyboard accessibility
-  const navDrops = navSections.querySelectorAll('.nav-drop');
-  if (isDesktop.matches) {
-    navDrops.forEach((drop) => {
-      if (!drop.hasAttribute('tabindex')) {
-        drop.setAttribute('role', 'button');
-        drop.setAttribute('tabindex', 0);
-        drop.addEventListener('focus', focusNavSection);
-      }
-    });
-  } else {
-    navDrops.forEach((drop) => {
-      drop.removeAttribute('role');
-      drop.removeAttribute('tabindex');
-      drop.removeEventListener('focus', focusNavSection);
-    });
-  }
-  // enable menu collapse on escape keypress
-  if (!expanded || isDesktop.matches) {
-    // collapse menu on escape press
-    window.addEventListener('keydown', closeOnEscape);
-  } else {
-    window.removeEventListener('keydown', closeOnEscape);
-  }
-}
-
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -100,6 +18,7 @@ export default async function decorate(block) {
   nav.id = 'nav';
   const topNav = document.createElement('div');
   topNav.className = 'top-nav';
+
   const topNavContent = document.createElement('div');
   topNavContent.className = 'top-nav-content';
   const topNavLeft = fragment.children[0];
@@ -119,6 +38,17 @@ export default async function decorate(block) {
   const primaryNav = fragment.children[0];
   primaryNav.className = 'primary-nav';
   nav.append(primaryNav);
+
+  // on scroll down equal to primaryNav height, set class "solid-nav" to primaryNav
+  const navHeight = 60;
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > navHeight) {
+      primaryNav.classList.add('solid-nav');
+    } else {
+      primaryNav.classList.remove('solid-nav');
+    }
+  });
+
   // add nav-drop class to nav items with dropdowns
   primaryNav.querySelectorAll('li').forEach((li) => {
     if (li.querySelector('ul')) {
@@ -133,6 +63,18 @@ export default async function decorate(block) {
       nav.append(secondaryNavBox);
     }
   });
+  const lastChild = primaryNav.querySelector('li:last-child');
+  lastChild.className = 'nav-drop';
+  lastChild.setAttribute('data-secondarynav', 'search-box');
+  lastChild.setAttribute('aria-expanded', 'false');
+  const searchBox = document.createElement('div');
+  searchBox.className = 'secondary-nav-box search-box';
+  searchBox.innerHTML = `<form action="https://www.famous-smoke.com/cigaradvisor/" class="search search--header lg-andUp">
+  <label class="sr-only" for="header-search-term">Search</label>
+  <input type="search" class="search__input predictiveSearch" id="header-search-term" maxlength="255" placeholder="Search here" name="s" autocomplete="off">
+  <button type="submit" class="search__submit" value="Submit" title="Submit">Submit</button>
+  </form>`;
+  nav.append(searchBox);
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);

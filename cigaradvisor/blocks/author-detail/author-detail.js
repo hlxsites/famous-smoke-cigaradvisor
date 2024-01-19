99a4615474783a08eee679f2236460b41e3422d8
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2024-01-19 13:41:02 -0500

    Feat/author template (#86)
    * Auto-block Author Template & add author detail block.
    
    * update author index.
    
    * Fix missing header h1.
    
    * Update author-detail.css
    
    * author details
    
    * Update author-detail.css
    
    * variables
    
    * Feature/404 (#79)
    
    * hero
    
    * initial commit
    
    * errorpage
    
    * Update hero.js
    
    * Revert "Update hero.js"
    
    This reverts commit 5cd8a8578f2788bfe81927138d5f894a3af8d688.
    
    * Revert "hero"
    
    This reverts commit 5e3525ecacd524d5e1637f34927fdbb09fc113c7.
    
    ---------
    
    Co-authored-by: Bryan Stopp <bstopp@users.noreply.github.com>
    
    * Dont need author template logic any more.
    
    * Fix a bit of the social stuff.
    
    * Fix some minor details on author detail block.
    
    * Styling fixes for image.
    
    * Double method declaration?
    
    * Empty list css file for now.
    
    * Fix bg image for performance.
    
    * Linting.
    
    * Eager load profile image.
    
    * Eager load lcp  image.
    
    ---------
    
    Co-authored-by: Bryan Stopp <bryan.stopp@gmail.com>
    Co-authored-by: Bryan Stopp <bstopp@users.noreply.github.com>

---
+++ cigaradvisor/blocks/author-detail/author-detail.js
@@ -0,0 +1,68 @@
+import { readBlockConfig, decorateIcons, createOptimizedPicture } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const config = readBlockConfig(block);
+  const social = block.querySelector('ul');
+  const picture = block.querySelector('picture');
+  const img = picture.querySelector('img');
+  const ratio = (parseInt(img.height, 10) / parseInt(img.width, 10)) * 100;
+  picture.style.paddingBottom = `${ratio}%`;
+
+  if (social) {
+    const links = social.querySelectorAll('a');
+    links.forEach((link) => {
+      const text = link.textContent;
+      link.textContent = '';
+      const textToClass = text.trim().toLowerCase().replace(/\s/g, '-');
+      link.setAttribute('title', `${config.name} on ${text}`);
+      if (textToClass && textToClass !== '') {
+        let icon;
+        switch (textToClass) {
+          case 'twitter':
+          case 'x':
+          case 'x-twitter':
+            icon = 'x-twitter';
+            break;
+          case 'facebook':
+            icon = 'facebook-f';
+            break;
+          case 'instagram':
+            icon = 'instagram';
+            break;
+          case 'youtube':
+            icon = 'youtube';
+            break;
+          case 'pintrest':
+            icon = 'pintrest-p';
+            break;
+          default:
+            icon = 'unknown';
+        }
+        link.innerHTML = `<span class="icon icon-${icon}"></span>`;
+      }
+    });
+  }
+  block.innerHTML = `
+    <div class="author-detail-content">
+      <div class="author-image">
+        ${picture.outerHTML}
+      </div>
+      <div class="author-info">
+      <div class="author-heading-wrapper">
+        <h2 class="author-name">${config.name}</h2>
+        <p class="author-title">${config.title || ''}</p>
+        </div>
+        <div class="author-intro">
+          <p>${config.intro}</p>
+        </div>
+        <div class="social-links">
+          ${social ? social.outerHTML : ''}
+        </div>
+      </div>
+    </div>
+  `;
+  const bg = createOptimizedPicture('https://main--famous-smoke-cigaradvisor--hlxsites.hlx.page/cigaradvisor/images/backgrounds/author.jpg', '', true);
+  bg.classList.add('bg-image');
+  block.append(bg);
+  decorateIcons(block);
+}

7dfb3fd0b1308987f68c90c558cb471d25fcd72e
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-01-23 19:02:18 -0500

    author-list

--- cigaradvisor/blocks/author-list/author-list.js
+++ cigaradvisor/blocks/author-list/author-list.js
@@ -6,22 +6,26 @@ import { generatePagination } from '../../scripts/util.js';
 export default async function decorate(block) {
   await loadCSS(`${window.hlx.codeBasePath}/blocks/author-teaser/author-teaser.css`);
   const configs = readBlockConfig(block);
-  // eslint-disable-next-line prefer-const
-  let { curatedauthors, limit = 10 } = configs;
+  const { curatedauthors, limit = 10 } = configs;
   let currentPage = 1;
   block.innerHTML = '';
 
   const urlParams = new URLSearchParams(window.location.search);
   currentPage = urlParams.get('page') ? parseInt(urlParams.get('page'), 10) : 1;
 
-  const allAuthors = await getAllAuthors(true);
+  let allAuthors = await getAllAuthors(true);
 
-  console.log('allAuthors', JSON.stringify(allAuthors));
-  console.log('curatedauthors', JSON.stringify(curatedauthors));
+  const curatedAuthorsInfo = [];
+  [...allAuthors].forEach((author) => {
+    if (curatedauthors.some((curatedAuthor) => curatedAuthor.includes(author.path))) {
+      curatedAuthorsInfo.push(author);
+      allAuthors.splice(allAuthors.indexOf(author), 1);
+    }
+  });
+
+  allAuthors = [...curatedAuthorsInfo, ...allAuthors];
   const totalAuthors = allAuthors.length;
   const totalPages = Math.ceil(totalAuthors / limit);
-  const curatedAuthorWrapper = document.createElement('div');
-  curatedAuthorWrapper.classList.add('curated-author-teaser-wrapper');
   [...allAuthors].slice((currentPage - 1) * limit, currentPage * limit).map(async (author) => {
     const authorTeaserWrapper = document.createElement('div');
     authorTeaserWrapper.classList.add('author-teaser-wrapper');
@@ -30,13 +34,8 @@ export default async function decorate(block) {
     authorTeaser.classList.add('block');
     authorTeaserWrapper.append(authorTeaser);
     buildAuthorTeaser(authorTeaser, author, true);
-    if (curatedauthors.some((curatedAuthor) => curatedAuthor.includes(author.path))) {
-      curatedAuthorWrapper.append(authorTeaserWrapper);
-    } else {
-      block.append(authorTeaserWrapper);
-    }
+    block.append(authorTeaserWrapper);
   });
-  block.prepend(curatedAuthorWrapper);
 
   if (totalPages > 1) {
     const pageinationContainer = document.createElement('div');

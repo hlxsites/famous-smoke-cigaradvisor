64fef85be4e65b99868367127e95d394d466cc84
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2024-02-22 11:52:14 -0500

    Feat/article rec (#226)
    * add promotion banner block
    
    * improve image fetching
    
    * recommendations
    
    * heading
    
    * building article-list
    
    * Update blog-post.js
    
    * Update blog-post.js
    
    * Update blog-post.css
    
    * Clean up article list a little.
    
    * Remove margin update
    
    * Update blog-post.js
    
    * Update blog-post.js
    
    * Update blog-post.js
    
    * Update blog-post.js
    
    ---------
    
    Co-authored-by: Luca <adobe@gituser.de>
    Co-authored-by: Bryan Stopp <bryan.stopp@gmail.com>

--- cigaradvisor/scripts/templates/blog-post.js
+++ cigaradvisor/scripts/templates/blog-post.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 import {
-  decorateExternalLink, fetchAuthorInfo, fetchCategoryInfo, fetchPostsInfo,
+  decorateExternalLink, fetchAuthorInfo, fetchCategoryInfo, fetchPostsInfo, loadPosts,
 } from '../scripts.js';
 import { buildBlock, getMetadata } from '../aem.js';
 import { addLdJsonScript } from '../linking-data.js';
@@ -88,8 +89,43 @@ export default async function decorate(main) {
   div.append(articleHeaderBlockEl);
   main.prepend(div);
 
-  const section = document.createElement('div');
-  section.append(buildBlock('article-navigation', []));
-  main.append(section);
+  const articleList = document.createElement('div');
+  const ul = document.createElement('ul');
+  const posts = await loadPosts();
+  // 2 posts from the same author “most recent by published date”
+  const authorPosts = posts.filter((post) => authorLink.includes(post.author) && post.path !== window.location.pathname);
+  if (authorPosts) {
+    if (authorPosts[0]) ul.innerHTML = `<li><a href="${authorPosts[0].path}">${authorPosts[0].path}</a></li>`;
+    if (authorPosts[1]) ul.innerHTML += `<li><a href="${authorPosts[1].path}">${authorPosts[1].path}</a></li>`;
+  }
+  // 1 post from the same category “most recent by published date”
+  const categoryPosts = posts.filter((post) => category.includes(post.category) && !authorPosts.includes(post) && post.path !== window.location.pathname);
+  if (categoryPosts && categoryPosts[0]) {
+    ul.innerHTML += `<li><a href="${categoryPosts[0].path}">${categoryPosts[0].path}</a></li>`;
+  }
+  // 1 random post from must reads page
+  const resp = await fetch('/cigaradvisor/must-reads.plain.html');
+  if (resp.ok) {
+    const dom = document.createElement('main');
+    dom.innerHTML = await resp.text();
+    const mustReadArticleList = dom.querySelectorAll('.article-list li a');
+    const mustReadArticles = Array.from(mustReadArticleList).map((a) => a.getAttribute('href'));
+    const randomMustReadArticles = mustReadArticles.filter((post) => !authorPosts.includes(post) && !categoryPosts.includes(post) && post.path !== window.location.pathname);
+    const randomIndex = Math.floor(Math.random() * randomMustReadArticles.length);
+    if (randomIndex && randomMustReadArticles[randomIndex]) ul.innerHTML += `<li><a href="${randomMustReadArticles[randomIndex]}">${randomMustReadArticles[randomIndex]}</a></li>`;
+  }
+
+  articleList.append(ul);
+  const articleListBlock = buildBlock('article-list', [['Articles', articleList]]);
+  const recommendationsHeading = document.createElement('h3');
+  recommendationsHeading.innerHTML = 'You Might Also Like...';
+
+  const articleListSection = document.createElement('div');
+  articleListSection.append(recommendationsHeading, articleListBlock);
+  main.append(articleListSection);
+
+  const navSection = document.createElement('div');
+  navSection.append(buildBlock('article-navigation', []));
+  main.append(navSection);
   addLdJson();
 }

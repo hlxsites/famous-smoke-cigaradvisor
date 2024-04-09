848803ca092ef2a749b265c326231007a76766bf
Bryan Stopp <bryan.stopp@gmail.com>
2024-04-09 15:25:30 -0400

    minor changes to loading transition.

--- cigaradvisor/blocks/search-results/search-results.js
+++ cigaradvisor/blocks/search-results/search-results.js
@@ -13,19 +13,7 @@ async function handleSearch(searchValue, block, limit) {
     wrapper.replaceChildren(searchSummary);
     return;
   }
-
-  // show loading spinner
-  const loadingImageContainer = document.createElement('div');
-  loadingImageContainer.classList.add('loading-image-container');
-  const loadingImage = document.createElement('img');
-  loadingImage.classList.add('loading-image');
-  loadingImage.src = '/cigaradvisor/images/search/ca-search-results-loading.svg';
-  loadingImageContainer.append(loadingImage);
-  const spinner = document.createElement('span');
-  spinner.classList.add('loader');
-  loadingImageContainer.append(spinner);
-  block.prepend(loadingImageContainer);
-
+  block.classList.add('loading');
   const worker = new Worker(`${window.hlx.codeBasePath}/blocks/search-results/search-worker.js`);
   worker.onmessage = async function handleWorker(event) {
     const { results } = event.data;
@@ -38,7 +26,7 @@ async function handleSearch(searchValue, block, limit) {
       noResults.textContent = 'Sorry, we couldn\'t find the information you requested!';
       wrapper.replaceChildren(searchSummary);
       wrapper.append(noResults);
-      loadingImageContainer.style.display = 'none';
+      block.classList.remove('loading');
       return;
     }
     let filteredDataCopy = [...results];
@@ -46,14 +34,8 @@ async function handleSearch(searchValue, block, limit) {
     // load the first page of results
     let resultsToShow = filteredDataCopy.slice(0, limit);
     // eslint-disable-next-line max-len
-    const articleListWrapper = block.querySelector('.article-list-wrapper');
-    loadingImageContainer.classList.add('partial-fade-out');
-    articleListWrapper.classList.add('partial-fade-in');
     await renderPage(wrapper, resultsToShow, limit, articlesCount);
-    articleListWrapper.classList.remove('partial-fade-in');
-    articleListWrapper.classList.add('fade-in');
-    loadingImageContainer.classList.remove('partial-fade-out');
-    loadingImageContainer.classList.add('fade-out');
+    block.classList.remove('loading');
 
     wrapper.prepend(searchSummary);
 
@@ -86,6 +68,17 @@ export default async function decorate(block) {
   const configs = readBlockConfig(block);
   const limit = Number.isNaN(parseInt(configs.limit, 10)) ? 10 : parseInt(configs.limit, 10);
 
+  // show loading spinner
+  const loadingImageContainer = document.createElement('div');
+  loadingImageContainer.classList.add('loading-image-container');
+  const loadingImage = document.createElement('img');
+  loadingImage.classList.add('loading-image');
+  loadingImage.src = '/cigaradvisor/images/search/ca-search-results-loading.svg';
+  loadingImageContainer.append(loadingImage);
+  const spinner = document.createElement('span');
+  spinner.classList.add('loader');
+  loadingImageContainer.append(spinner);
+
   const articleListWrapper = document.createElement('div');
   articleListWrapper.classList.add('article-list-wrapper');
 
@@ -96,10 +89,11 @@ export default async function decorate(block) {
   articleTeaserWrapper.classList.add('article-teaser-wrapper');
 
   articleList.append(articleTeaserWrapper);
-
   articleListWrapper.append(articleList);
 
-  block.replaceChildren(articleListWrapper);
+  block.replaceChildren(loadingImageContainer, articleListWrapper);
+
+
 
   const heroSearch = document.querySelector('.hero-search');
 
